c41e767e4ee74defd5af69688162309883ce3a8c
Loay Ashraf <loay.ashraf.96@gmail.com>
2024-01-09 00:29:33 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:expense_tracker/app/expense_tracker_app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ExpenseTrackerApp());
+    await tester.pumpWidget(const ExpenseTrackerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
